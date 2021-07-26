77aab224088e797e7e121ed6c71bb8c010ed8d9a
Ankit <ankitmaurya0225@gmail.com>
2021-07-26 16:46:16 +0530

    added users

---
+++ src/controllers/users.js
@@ -0,0 +1,23 @@
+
+const {Users} = require('../db/models')
+const { generateRandomUser } = require('../utils/username')
+
+async function createAnnonymusUser() {
+
+    const user = await Users.create({
+        username: generateRandomUser()
+    })
+
+    return user
+}
+// testing is commpulsary
+async function task () {
+    console.log(await createAnnonymusUser());
+    console.log('===============');
+    console.log(await createAnnonymusUser());
+    console.log('===============');
+    console.log(await createAnnonymusUser());
+    console.log('===============');
+}
+
+task()
