4adb055e7ce9bb55ff353221ea488f7805ca9525
Ankit <ankitmaurya0225@gmail.com>
2021-07-29 00:36:20 +0530

    adhura code

--- src/routes/posts/index.js
+++ src/routes/posts/index.js
@@ -1,13 +1,30 @@
 const {Router} = require('express')
 
+const {showPosts, 
+    CreateUserPost
+} = require('../../controllers/posts')
+
 const route = Router()
 
 
-route.get('/', (req,res) => {
-    res.send("All Posts Here")
+route.get('/', async (req,res) => {
+    const posts = await showPosts() 
+    res.status(200).send(posts)
 })
 
+route.post('/',  async (req, res) => {
+    const { userId, title , body } = req.body
+
+    if((!userId) || (!title) || (!body)){
+        return res.status(400).send({
+            error: 'Need UserId, title and body to create Post'
+        })
+    }
+
+    const post = await CreateUserPost(userId, title, body)
+    res.status(201).send(post)
+})
 
 module.exports = {
-    PostsRoute :route
+    PostsRoute: route
 }
--- src/routes/server.js
+++ src/routes/server.js
@@ -8,8 +8,8 @@ const {PostsRoute} = require('../routes/posts')
 const app = express()
 
 
-// app.use(express.json())
-// app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 
 app.use('/api/users', UsersRoute)
