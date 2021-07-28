d129b3162a54beaac623939d1bb1ed68f2ce7898
Ankit <ankitmaurya0225@gmail.com>
2021-07-28 20:42:23 +0530

    added path

--- src/routes/server.js
+++ src/routes/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const path = require('path')
 const {db} = require('../db/models')
+const {UsersRoute} = require('../routes/users')
+const {PostsRoute} = require('../routes/posts')
 const app = express()
+
+
+app.use('/api/users', UsersRoute)
+app.use('/api/posts', PostsRoute)
+
+app.use('/', express.static(__dirname + '/public'))
 
 // db.sync({force: true})
 db.sync()
