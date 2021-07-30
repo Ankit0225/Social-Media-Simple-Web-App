82641253632c1e1112a39208dabab6f3dace149e
Ankit <ankitmaurya0225@gmail.com>
2021-07-30 20:41:49 +0530

    testing
    Signed-off-by: Ankit <ankitmaurya0225@gmail.com>

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -29,27 +29,27 @@ module.exports = {
 // test code 
 
 // async function test () {
-//     // console.log(
-//     //     await CreateUserPost(
-//     //         1,
-//     //         'This is a sample Post',
-//     //         'Body of this post will be here'
-//     //     )
-//     // ),
-//     // console.log(
-//     //     await CreateUserPost(
-//     //         2,
-//     //         'Another sample Post',
-//     //         'Body of this post will be here'
-//     //     )
-//     // ),
-//     // console.log(
-//     //     await CreateUserPost(
-//     //         3,
-//     //         'This is a sample Post',
-//     //         'Body of this post will be here'
-//     //     )
-//     // )
+// console.log(
+//     await CreateUserPost(
+//         1,
+//         'This is a sample Post',
+//         'Body of this post will be here'
+//     )
+// ),
+// console.log(
+//     await CreateUserPost(
+//         2,
+//         'Another sample Post',
+//         'Body of this post will be here'
+//     )
+// ),
+// console.log(
+//     await CreateUserPost(
+//         3,
+//         'This is a sample Post',
+//         'Body of this post will be here'
+//     )
+// )
 //  const posts = await showPosts()
 //  for(let p of posts)
 //  console.log(`${p.title}\nauthor: ${p.user.username}\n${p.body}\n============\n`);
