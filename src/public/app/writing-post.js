50a11fcbfdc7f95499dc88c44d9022658f599b1d
Ankit <ankitmaurya0225@gmail.com>
2021-09-10 20:03:40 +0530

    wrong commit code

---
+++ src/public/app/writing-post.js
@@ -0,0 +1,14 @@
+$(() => {
+    $('#Post-btn').click(() => {
+        const UserId = JSON.parse(window.localStorage.user).id
+        const title = $('#p-title').val()
+        const body = $('#p-body').val()
+      
+    })
+
+    $.post('/api/posts', { userId, title, body }, (data) => {
+        $('#content').load('/components/my-posts.html')
+        $('.nav-item .active').removeClass('active')
+        $("[data-components='my-posts']").addClass('active')
+      })
+})
